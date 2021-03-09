2089c90e2a31e14d9b04b7230bdf62e3eeb2faa3
Eric Aguiló Soler <72026220+eragso@users.noreply.github.com>
2021-03-09 18:29:49 +0100

    Intento DetailPage

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -18,11 +18,12 @@ class _DetailPageState extends State<DetailPage> {
       body: Stack(
         children: <Widget>[
           Container(
-            height: MediaQuery.of(context).size.height,
+            color: Colors.blue,
+            /*height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage(widget.imgPath), fit: BoxFit.fill)),
+                    image: AssetImage(widget.imgPath), fit: BoxFit.fill)),*/
           ),
           Padding(
             padding: EdgeInsets.only(top: 25),
@@ -51,51 +52,129 @@ class _DetailPageState extends State<DetailPage> {
               ],
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 340,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  ),
-                  color: Colors.blue,
-                ),
+            Padding(
+                padding: EdgeInsets.only(left: 15, top: 5, right: 15),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.only(top: 5, left: 20, right: 20),
-                        child: Divider(
-                          color: Colors.white,
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, right: 10),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "Day Schedule",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Spacer(),
-                          Switch(
-                            value: tempValue,
-                            onChanged: (newVal) {
-                              setState(() {
-                                tempValue = newVal;
-                                print(newVal);
-                              });
-                            },
-                            activeColor: Colors.blue,
-                          )
-                        ],
-                      ),
-                    ),
+                    Container(
+                      height: 241,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 5.0, color: Colors.red),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue[900],
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text('We   '),
+                              ]
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text('We move under cover and we move as one'),
+                                Text('We move under cover and we move as one'),
+                              ]
+                            ),
+                            ],
+                        ),
+                        ), 
                   ],
-                )),
-          )
+                ), 
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15, top: 5, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 241,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 5.0, color: Colors.red),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue[900],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Text('Ataques', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,)),
+                            Row( children: [
+                            Column(
+                              children: <Widget>[
+                                Text('Nombre'),
+                                Text('Nombre'),
+                                Text('Nombre'),
+                                Text('Nombre'),
+                              ]
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text('Land'),
+                              ]
+                            ),], ),
+                            ],
+                        ),
+                        ), 
+                  ],
+                ), 
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15, top: 5, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: 10, top: 15, right: 10, bottom: 15),
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 5.0, color: Colors.red),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue[900],
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                          height: 80,
+                          width: 300,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                              side: BorderSide(color: Colors.red)
+                            ),
+                            color: Colors.red,
+                            textColor: Colors.yellow,
+                            child: Text("Subir", style: TextStyle(fontSize: 30.0,),
+                            ),
+ 
+                          )
+                            ),
+                          ], 
+                        ), 
+                        ), 
+                  ],
+                ), 
+              ),
         ],
+      ),
+    );
+  }
+  Widget listCard(String imgpath) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => DetailPage(imgPath: imgpath,)));
+      },
+      child: Container(
+        width: 70,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          image: DecorationImage(image: AssetImage(imgpath), fit: BoxFit.fill),
+        ),
       ),
     );
   }
